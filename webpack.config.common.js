a13d90dd64b250944ca90dcb199724f36707e9bc
Tommy Han <tommyhan@rocketauto.com>
2022-04-24 08:32:48 -0400

    initial version

---
+++ webpack.config.common.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // change the entry to use different app.
+  entry: "./src/functionalAstronauts/index.js",
+  output: {
+    filename: "index.bundle.js",
+    path: path.join(__dirname, "/dist"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "[name].html",
+      inject: true,
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ webpack.config.dev.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.config.common");
+
+module.exports = merge(commonConfig, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+});
